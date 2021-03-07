1f3bddf6d872b9c078eba5af7ad0d964e455ed84
nely de faria <nelly.nf54@gmail.com>
2021-03-07 16:12:34 -0300

    Adiciona arquivo banco-de-dados.php

---
+++ php-oo-projeto-final/config/banco-de-dados.php
@@ -0,0 +1,12 @@
+<?php
+
+
+return [
+    'driver' => 'pgsql',
+    'servidor' => 'localhost',
+    'banco-de-dados' => 'treinaweb',
+    'usuario' => 'postgres',
+    'senha' => '704802',
+    'modo-de-erro' => PDO::ERRMODE_EXCEPTION,
+    'conexao-persistente' => true
+];
