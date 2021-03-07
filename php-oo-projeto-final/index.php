17ec8f1848dfa75be5cf9b35cf7cbd0deb2e6746
nely de faria <nelly.nf54@gmail.com>
2021-03-07 16:20:53 -0300

    Fazendo tratamento de exceções

--- php-oo-projeto-final/index.php
+++ php-oo-projeto-final/index.php
@@ -3,8 +3,20 @@
 require_once "autoload/autoload.php";
 
 use App\Repositorio\Curso;
+use App\Uteis\Conexao;
 
-$cursoRepositorio = new Curso;
+try {
+    $configBancoDados = require 'config/banco-de-dados.php';
+    $conexao = new Conexao($configBancoDados);
+
+    $cursoRepositorio = new Curso($conexao);
+    $cursos = $cursoRepositorio->todos();
+
+} catch (PDOException $PDOException) {
+    echo $PDOException->getMessage();
+    exit();
+}
+
 
 require_once "exibicao/principal.php";
 
